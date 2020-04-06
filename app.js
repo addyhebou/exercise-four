c75b41ddc53db5c04429546b3272c06e3260c91e
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-04-06 09:29:16 -0400

    You don't love him if you're thinking of me

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const express = require('express');
 // Initiate Express to an App
 const app = express();
 // Set the port
-// const port = 4000;
+const port = process.env.PORT || 4000;
+
+const firebase = require("firebase/app")
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWXx0pNwcNZm6XAFLttmG2lNH3U8udWnY",
+    authDomain: "exercise-four-7ef88.firebaseapp.com",
+    databaseURL: "https://exercise-four-7ef88.firebaseio.com",
+    projectId: "exercise-four-7ef88",
+    storageBucket: "exercise-four-7ef88.appspot.com",
+    messagingSenderId: "677328572353",
+    appId: "1:677328572353:web:787ba66ac11f4a221b2940"
+  };
 
 const port = process.env.PORT || 4000;
 
