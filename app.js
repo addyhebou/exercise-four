92129bbfa3b90131a374e22fef152350061f92ee
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2020-04-08 09:26:53 -0400

    wednesday april 8

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express');
 // Initiate Express to an App
 const app = express();
 // Set the port
-const port = process.env.PORT || 4000;
+const firebase = require("firebase")
 
-const firebase = require("firebase/app")
+
+const port = process.env.PORT || 4000;
 
 const firebaseConfig = {
     apiKey: "AIzaSyCWXx0pNwcNZm6XAFLttmG2lNH3U8udWnY",
@@ -17,15 +18,36 @@ const firebaseConfig = {
     appId: "1:677328572353:web:787ba66ac11f4a221b2940"
   };
 
-const port = process.env.PORT || 4000;
+
+firebase.initializeApp(firebaseConfig);
+
+// Initialize firestore database
+const db = firebase.firestore();
+
+// Create empty ARray
+const blogpostsArray = [];
+// Get Blog Posts
+const blogposts = db
+  .collection('blogposts')
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      // Push document into array every time the query loops over existing articles
+      blogpostsArray.push(doc.data());
+    });
+  })
+  .catch(function(error){
+    console.log('Errors: ', error);
+  });
 
 // Create Base Route
+// Send JSON array as response
 app.get('/', (req, res) => 
-    res.send('Exercise Four')
-
+    res.send(blogpostsArray)
 );
 
 // Set up route
 app.listen(port, () =>
  console.log(`Example app listening at http://localhost:${port}`)
- );
+);
